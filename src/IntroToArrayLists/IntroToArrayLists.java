58b95a24339ae8c947f41e7351793f6d58260988
League <league@Mac-mini.local>
2018-01-10 18:28:33 -0800

    I did some Arry list stuff

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,14 +1,24 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		
+		ArrayList<String> names = new ArrayList<String>();
+		
 		//   Don't forget to import the ArrayList class
 		
 		//2. Add five Strings to your list
-		
+		names.add("Coach");
+		names.add("Jack");
+		names.add("Roberto");
+		names.add("Nancy");
 		//3. Print all the Strings using a standard for-loop
-		
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(names.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
