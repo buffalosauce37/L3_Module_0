ad5394a88d5ea00e02ab35e49e3d672272dba706
tactreconx <league@WTS-MM-11.local>
2018-01-24 18:26:05 -0800

    I did for loop stuff.

--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -15,16 +15,33 @@ public class IntroToArrayLists {
 		names.add("Jack");
 		names.add("Roberto");
 		names.add("Nancy");
+		names.add("Mike");
 		//3. Print all the Strings using a standard for-loop
 		for (int i = 0; i < names.size(); i++) {
 			System.out.println(names.get(i));
 		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String i : names) {
+		System.out.println(i);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for (int i = 0; i < names.size(); i++) {
+			if(i%2 == 0) {
+				System.out.println(names.get(i));
+			}
+
+		}
 		//6. Print all the Strings in reverse order.
-		
+		System.out.println("");
+		for (int i = names.size()-1; i >=0; i--) {
+			System.out.println(names.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		System.out.println("");
+		for(String name : names) {
+			if(name.contains("e")) {
+				System.out.println(name);
+			}
+		}
 	}
 }
